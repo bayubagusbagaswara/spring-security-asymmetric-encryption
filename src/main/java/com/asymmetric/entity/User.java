42fc02b1b472a3374c0e4b4d33631d895696c944
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-31 18:03:15 +0700

    add entity User

---
+++ src/main/java/com/asymmetric/entity/User.java
@@ -0,0 +1,87 @@
+package com.asymmetric.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "is_enabled")
+    private boolean enabled;
+
+    @Column(name = "is_locked")
+    private boolean locked;
+
+    @Column(name = "is_credentials_expired")
+    private boolean expired;
+
+    @Column(name = "is_email_verified")
+    private boolean emailVerified;
+
+    @Column(name = "is_phone_verified")
+    private boolean phoneVerified;
+
+    @Column(name = "credentials_expired")
+    private boolean credentialsExpired;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {
+                    @JoinColumn(name = "user_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "role_id")
+            }
+    )
+    private List<Role> roles;
+
+}

0cce2c4b0e6bc46cb0ca5b82dfd94ef53fce372a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-31 18:02:20 +0700

    add Role

---
+++ src/main/java/com/asymmetric/entity/Role.java
@@ -0,0 +1,18 @@
+package com.asymmetric.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "roles")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+}

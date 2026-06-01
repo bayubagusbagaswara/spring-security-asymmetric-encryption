5450f599cfbba71917314f7fd954d551d49a461a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-06-01 16:31:55 +0700

    add RoleRepository

---
+++ src/main/java/com/asymmetric/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.asymmetric.repository;
+
+import com.asymmetric.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+    Optional<Role> findByName(String roleUser);
+
+}

7c0b5e70a5af59ba9c84a02855537c2d6a73eda1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-06-01 16:32:24 +0700

    add UserRepository

---
+++ src/main/java/com/asymmetric/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.asymmetric.repository;
+
+import com.asymmetric.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}

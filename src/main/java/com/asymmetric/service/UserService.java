7cbb9f55b9934b553fe4c2ce2bafda3918248059
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-06-01 16:32:30 +0700

    add UserService

---
+++ src/main/java/com/asymmetric/service/UserService.java
@@ -0,0 +1,19 @@
+package com.asymmetric.service;
+
+import com.asymmetric.dto.user.ChangePasswordRequest;
+import com.asymmetric.dto.user.ProfileUpdateRequest;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    void updateProfileInfo(ProfileUpdateRequest request, String userId);
+
+    void changePassword(ChangePasswordRequest request, String userId);
+
+    void deactivateAccount(String userId);
+
+    void reactivateAccount(String userId);
+
+    void deleteAccount(String userId);
+
+}

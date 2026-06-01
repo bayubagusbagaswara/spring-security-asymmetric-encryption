b2a488977dbdb4232a452b55e542ca75c75fbea2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-06-01 16:23:44 +0700

    add AuthenticationRequest

---
+++ src/main/java/com/asymmetric/dto/auth/AuthenticationRequest.java
@@ -0,0 +1,20 @@
+package com.asymmetric.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationRequest {
+
+    @NotBlank(message = "VALIDATION.AUTHENTICATION.EMAIL.NOT_BLANK")
+    @Email(message = "VALIDATION.AUTHENTICATION.EMAIL.FORMAT")
+    private String email;
+
+    @NotBlank(message = "VALIDATION.AUTHENTICATION.PASSWORD.NOT_BLANK")
+    private String password;
+}

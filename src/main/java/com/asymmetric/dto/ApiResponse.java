ccadb9ede7229f53df8740b2c010c4d604188cab
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-06-16 13:24:21 +0700

    add ApiResponse

---
+++ src/main/java/com/asymmetric/dto/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.asymmetric.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private Integer httpStatus;
+    private String code;
+    private String message;
+    private T data;
+
+}

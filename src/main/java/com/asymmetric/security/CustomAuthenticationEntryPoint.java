42c83116ce7fdc79da6ca5e59b199cbc5eceae23
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-07-02 19:57:10 +0700

    add CustomAuthenticationEntryPoint

---
+++ src/main/java/com/asymmetric/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.asymmetric.security;
+
+import com.asymmetric.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("UNAUTHORIZED")
+                .message("Authentication is required")
+                .build();
+
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+
+}
